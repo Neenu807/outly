f13c35e29f1017e59b8fa9e2db554be3b8c5789a
Neenu M R <neenuravindradas807@gmail.com>
2026-09-12 21:53:50 +0530

    completed phase 0 foundation

--- client/src/shared/components/Navbar.jsx
+++ client/src/shared/components/Navbar.jsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Outly</Link>
-        <Link to="/activities">Activities</Link>
-      </nav>
+    <header className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          Outly
+        </Link>
+
+        <nav className="navbar-links">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "navbar-link active" : "navbar-link"
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/activities"
+            className={({ isActive }) =>
+              isActive ? "navbar-link active" : "navbar-link"
+            }
+          >
+            Activities
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
